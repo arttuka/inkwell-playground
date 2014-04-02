941fe769aa541d104d8197ef8f8dcb3f9fac11af
Timo Mihaljov <timo.mihaljov@solita.fi>
2014-04-02 11:50:19 +0300

    Upgrade Inkwell

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [inkwell "0.1.0"]]
+                 [inkwell "0.1.1"]]
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[org.clojure/tools.namespace "0.2.4"]]}})
