447b06515726d4203195619887b606a7abc437a6
Arttu Kaipiainen <arttu.kaipiainen@solita.fi>
2014-04-09 15:27:56 +0300

    newtonian physics

--- src/inkwell_playground/core.clj
+++ src/inkwell_playground/core.clj
@@ -8,56 +8,68 @@
   [min max]
   (+ min (* (- max min) (rand))))
 
+(defn vector-length [[x y]]
+  (sqrt (+ (* x x) (* y y))))
+
+(defn +v [[x1 y1] [x2 y2]]
+  [(+ x1 x2)
+   (+ y1 y2)])
+
+(defn rotated
+  ([[x y] rotation]
+    (rotated [0 0] [x y] rotation))
+  ([[x0 y0] [x1 y1] rotation]
+    (let [s (sin rotation)
+          c (cos rotation)
+          dx (- (* x1 c) (* y1 s))
+          dy (+ (* x1 s) (* y1 c))]
+      [(+ x0 dx) (+ y0 dy)])))
+
+(defn new-meteor [position type]
+  {:position position
+   :type type
+   :rotation 0
+   :rotation-speed (case type
+                     :big (rand-float 0 0.1)
+                     :small (rand-float 0.1 0.2))
+   :speed (case type
+            :big [(rand-float -1.5 1.5) (rand-float -1.5 1.5)]
+            :small [(rand-float -2.5 2.5) (rand-float -2.5 2.5)])
+   :size (case type
+           :big 50
+           :small 18)})
+
 ;; This is the state that the sketch starts with. You can return the sketch to
 ;; this state by pressing F12. See handle-event for how it's done!
 (def initial-state {:player {:position [300 400]
-                             :rotation 0
-                             :speed 0
-                             :moving :decelerate
-                             :direction 0
+                             :speed [0 0]
+                             :rotation HALF-PI
+                             :rotation-speed 0
+                             :moving false
                              :turning nil
-                             :side :left}
-                    :meteors [{:position [300 100]
-                              :rotation 0
-                              :direction (rand-float 0 TWO-PI)
-                              :speed (rand-float 1 3)
-                              :type :big}
-                              {:position [500 300]
-                              :rotation 0
-                              :direction (rand-float 0 TWO-PI)
-                              :speed (rand-float 1 3)
-                              :type :big}
-                              {:position [1000 700]
-                              :rotation 0
-                              :direction (rand-float 0 TWO-PI)
-                              :speed (rand-float 1 3)
-                              :type :big}
-                              {:position [800 200]
-                              :rotation 0
-                              :direction (rand-float 0 TWO-PI)
-                              :speed (rand-float 1 3)
-                              :type :big}]
+                             :side :left
+                             :size 45}
+                    :meteors [(new-meteor [300 100] :big)
+                              (new-meteor [500 300] :big)
+                              (new-meteor [1000 700] :big)
+                              (new-meteor [800 200] :big)]
                     :bullets []
                     :tick-count 0})
 
 ;; These functions return new states based on the old state (and some additional
-;; arguments). Note that they are pure functions: they do not *modify* the
-;; state! They return new values without touching the old value.
+;; arguments). Note that they are pure functions: they do not *mod
+
+(defn move-item [{:keys [position speed rotation rotation-speed] :as item}]
+  (let [[size-x size-y] window-size
+        [new-x new-y] (+v position speed)
+        new-rotation (mod (+ rotation rotation-speed) TWO-PI)
+        ttl (some-> item :ttl dec)]
+    (cond-> (assoc item :position [(mod new-x size-x) (mod new-y size-y)]
+                        :rotation new-rotation)
+            ttl (assoc :ttl ttl))))
+
 (defn update-tick-count [state]
   (update-in state [:tick-count] inc))
-
-(defn item-rotated [[x0 y0] speed direction]
-  (let [dx (* speed (sin direction))
-        dy (* speed (cos direction))]
-    [(+ x0 dx) (- y0 dy)]))
-
-(defn move-item [item]
-  (let [{:keys [position direction speed]} item
-        [size-x size-y] window-size
-        [new-x new-y] (item-rotated position speed direction)
-        ttl (some-> item :ttl dec)]
-    (cond-> (assoc item :position [(mod new-x size-x) (mod new-y size-y)])
-            ttl (assoc :ttl ttl))))
 
 (defn update-meteors [state]
   (let [rotation (mod (/ (:tick-count state) 100) TWO-PI)]
@@ -65,39 +77,45 @@
       (update-in [:meteors] #(map (fn [meteor] (assoc meteor :rotation rotation)) %))
       (update-in [:meteors] #(map move-item %)))))
 
-(defn set-player-speed [state]
+(defn set-player-speed-newtonian [state]
   (let [speed (get-in state [:player :speed])
+        rotation (get-in state [:player :rotation])
+        rotation-speed (get-in state [:player :rotation-speed])
+        turning (get-in state [:player :turning])
         moving (get-in state [:player :moving])
-        new-speed (case moving
-                    :accelerate (min 10 (+ speed 0.2))
-                    :decelerate (max 0 (- speed 0.2)))]
-    (assoc-in state [:player :speed] new-speed)))
+        new-speed (if moving
+                    (+v speed (rotated [0 -0.2] rotation))
+                    speed)
+        new-rotation-speed (case turning
+                             :left (- rotation-speed 0.001)
+                             :right (+ rotation-speed 0.001)
+                             rotation-speed)]
+    (-> state
+      (assoc-in [:player :speed] new-speed)
+      (assoc-in [:player :rotation-speed] new-rotation-speed))))
+
+(defn set-player-speed [state]
+  (let [speed (vector-length (get-in state [:player :speed]))
+        rotation (get-in state [:player :rotation])
+        turning (get-in state [:player :turning])
+        moving (get-in state [:player :moving])
+        new-speed (if moving
+                    (min 10 (+ speed 0.2))
+                    (max 0 (- speed 0.2)))
+        new-rotation-speed (case turning
+                             :left (* new-speed -0.01)
+                             :right (* new-speed 0.01)
+                             0)
+        new-speed-vector (rotated [0 (- new-speed)] rotation)]
+    (-> state
+      (assoc-in [:player :speed] new-speed-vector)
+      (assoc-in [:player :rotation-speed] new-rotation-speed))))
 
 (defn move-player [state]
   (update-in state [:player] move-item))
 
 (defn move-bullets [state]
   (update-in state [:bullets] #(map move-item %)))
-
-(defn rotated [[x0 y0] [x1 y1] rotation]
-  (let [s (sin rotation)
-        c (cos rotation)
-        dx (- (* x1 c) (* y1 s))
-        dy (+ (* x1 s) (* y1 c))]
-    [(+ x0 dx) (+ y0 dy)]))
-
-(defn rotate-player [state]
-  (let [{:keys [turning speed]} (:player state)]
-    (-> (case turning
-          :left (-> state
-                  (update-in [:player :direction] - (* speed 0.01))
-                  (update-in [:player :rotation] - (* speed 0.01)))
-          :right (-> state
-                  (update-in [:player :direction] + (* speed 0.01))
-                  (update-in [:player :rotation] + (* speed 0.01)))
-          state)
-      (update-in [:player :rotation] mod TWO-PI)
-      (update-in [:player :direction] mod TWO-PI))))
 
 (defn turn-player [state direction]
   (assoc-in state [:player :turning] direction))
@@ -106,13 +124,14 @@
   (assoc-in state [:player :moving] moving))
 
 (defn shoot [state]
-  (let [{:keys [side direction position]} (:player state)
+  (let [{:keys [side rotation position]} (:player state)
         bullet {:position (case side
-                            :left (rotated position [-45 -20] direction)
-                            :right (rotated position [45 -20] direction))
-                :rotation direction
-                :direction direction
-                :speed 20
+                            :left (rotated position [-45 -20] rotation)
+                            :right (rotated position [45 -20] rotation))
+                :rotation rotation
+                :rotation-speed 0
+                :speed (rotated [0 -30] rotation)
+                :size 0
                 :ttl 50}
         new-side (case side
                    :left :right
@@ -131,38 +150,30 @@
 
 (defn collides? [item1 item2]
   (let [[x0 y0] (:position item1)
-        [x1 y1] (:position item2)]
-    (> 5000 (squared-distance x0 y0 x1 y1))))
+        s1 (:size item1)
+        [x1 y1] (:position item2)
+        s2 (:size item2)]
+    (> (* (+ s1 s2) (+ s1 s2)) (squared-distance x0 y0 x1 y1))))
+
+(defn some-collides [item coll]
+  (some (partial collides? item) coll))
 
 (defn new-meteors-and-bullets [state]
   (let [new-meteors (apply concat (for [meteor (:meteors state)]
-                                  (if (some (partial collides? meteor) (:bullets state))
+                                  (if (some-collides meteor (:bullets state))
                                     (if (= :big (:type meteor))
-                                      (do
-                                        [{:position (:position meteor)
-                                         :type :small
-                                         :direction (rand-float 0 TWO-PI)
-                                         :rotation 0
-                                         :speed (rand-float 2 5)}
-                                        {:position (:position meteor)
-                                         :type :small
-                                         :direction (rand-float 0 TWO-PI)
-                                         :rotation 0
-                                         :speed (rand-float 2 5)}
-                                        {:position (:position meteor)
-                                         :type :small
-                                         :direction (rand-float 0 TWO-PI)
-                                         :rotation 0
-                                         :speed (rand-float 2 5)}])
-                                      nil)
+                                      [(new-meteor (:position meteor) :small)
+                                       (new-meteor (:position meteor) :small)
+                                       (new-meteor (:position meteor) :small)]
+                                      [])
                                     [meteor])))
-        new-bullets (remove #(some (partial collides? %) (:meteors state)) (:bullets state))]
+        new-bullets (remove #(some-collides % (:meteors state)) (:bullets state))]
     (assoc state :bullets new-bullets
                  :meteors new-meteors)))
 
 (defn check-collisions [state]
   (cond-> (new-meteors-and-bullets state)
-    (some (partial collides? (:player state)) (:meteors state))
+    (some-collides (:player state) (:meteors state))
       (dissoc :player)))
 
 ;; Like the functions above, handle-event is a pure function that returns a new
@@ -174,28 +185,31 @@
                    ;; When F12 is pressed, we return initial-state. This gives
                    ;; us a simple way to reset the sketch to a known state.
                    :f12 initial-state
-                   :up (start-stop-player state :accelerate)
+                   :up (start-stop-player state true)
                    :left (turn-player state :left)
                    :right (turn-player state :right)
                    ;; When any other key is pressed, we don't want to change the
                    ;; state, so we just return the old state.
                    (case (:key-code event)
                      32 (shoot state)
+                     10 (update-in state [:newtonian] not)
                      state))
     :key-released (case (:key-name event)
-                    :up (start-stop-player state :decelerate)
+                    :up (start-stop-player state false)
                     :left (turn-player state nil)
                     :right (turn-player state nil)
                     state)
-    :tick (-> state
-            update-tick-count
-            update-meteors
-            rotate-player
-            set-player-speed
-            move-player
-            move-bullets
-            cull-bullets
-            check-collisions)
+    :tick (let [set-player-speed-fn (if (:newtonian state)
+                                      #'set-player-speed-newtonian
+                                      #'set-player-speed)]
+            (-> state
+              update-tick-count
+              update-meteors
+              set-player-speed-fn
+              move-player
+              move-bullets
+              cull-bullets
+              check-collisions))
     state))
 
 ;; See directories:
@@ -240,7 +254,7 @@
   (background 0 0 0)
   (image-mode :center)
   (draw-component (:player state) (player-image (get-in state [:player :turning]))
-                  (when (= :accelerate (get-in state [:player :moving]))
+                  (when (get-in state [:player :moving])
                     jets))
   (doseq [meteor (:meteors state)
           :let [image (case (:type meteor)
